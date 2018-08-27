dec902fcde5233596dce689eee95ca010c8347ae
Zachary Marion <zfm2@duke.edu>
2018-08-26 18:54:09 -0700

    Remove references to new posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,6 +1,6 @@
 import { meta as welcomeMeta } from './welcome.mdx';
 import { meta as minimaxMeta } from './minimax.mdx';
-import { meta as reactSvgTreeMeta } from './react-svg-tree.mdx';
-import { meta as minimaxContinued } from './minimax-continued.mdx';
+// import { meta as reactSvgTreeMeta } from './react-svg-tree.mdx';
+// import { meta as minimaxContinued } from './minimax-continued.mdx';
 
-export { welcomeMeta, minimaxMeta, reactSvgTreeMeta, minimaxContinued };
+export { welcomeMeta, minimaxMeta };
