7468401748c42009aa0e73562ade0c372c1f95a8
Zachary Marion <zfm2@duke.edu>
2018-09-22 14:34:59 -0700

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Router from 'next/router';
 import { MDXProvider } from '@mdx-js/tag';
 import { Provider as RebassProvider } from 'rebass';
+import withGA from 'next-ga';
 
 import theme from 'constants/theme';
 import components from '../components/markdown';
 
-export default ({ Component, pageProps }) => (
+const App = ({ Component, pageProps }) => (
   <MDXProvider components={components}>
     <RebassProvider theme={theme}>
       <Component {...pageProps} />
     </RebassProvider>
   </MDXProvider>
 );
+
+export default withGA('UA-126250037-1', Router)(App);
