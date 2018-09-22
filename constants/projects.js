e195ef5425b6ae079fc745e0259755734d1e6b75
Zachary Marion <zfm2@duke.edu>
2018-09-22 13:55:39 -0700

    Update links to not point to root domain

--- constants/projects.js
+++ constants/projects.js
@@ -4,7 +4,7 @@ export default [
     description:
       'Built a React component that renders a well-formatted general tree graph based on a 1989 paper. The idea came from my first blog post on the minimax algorithm, which used tree visualizations heavily. I wanted a way to easily animate an svg tree to show the progression of an algorithm.',
     image: '/static/home/tree.png',
-    demoLink: 'http://zacharyfmarion.io/react-svg-tree/',
+    demoLink: 'https://zacharyfmarion.github.io/react-svg-tree/',
     sourceCodeLink: 'https://github.com/zacharyfmarion/react-svg-tree',
   },
   {
